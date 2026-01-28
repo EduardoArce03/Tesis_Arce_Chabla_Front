2b12cc48392f5928749df9f2646493970ede03c9
eduardo <eduardomaldonado2003ortiz@gmail.com>
2026-01-28 05:22:15 -0500

    updates

--- src/app/env/environment.ts
+++ src/app/env/environment.ts
@@ -4,8 +4,4 @@ export const environment = {
   apiUrl: 'http://localhost:18080/api'
 };
 
-// src/environments/environment.prod.ts
-export const environmentProd = {
-  production: true,
-  apiUrl: 'tesis_arce_chabla_back.railway.internal'
-};
+
